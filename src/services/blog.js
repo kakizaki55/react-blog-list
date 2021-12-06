0ed6750ca466b12a97bf8d869ce2891c6138f6ce
minoka kakizaki <minoka.kakizaki@gmail.com>
2021-12-06 15:30:58 -0800

    authors rendering properly

--- src/services/blog.js
+++ src/services/blog.js
@@ -1,6 +1,10 @@
 import { checkError, client } from './client';
 
 export async function getBlogs() {
-  const blogs = await client.from('blogs').select();
+  const blogs = await client.from('blogs').select(`*, authors(name)`);
   return checkError(blogs);
 }
+// export async function getAuthorByID() {
+//   const authorName = await client.from('blog').select(`authog_id(name)`);
+//   return checkError(authorName);
+// }
