369a6ae05bc611e6b7deb883e93557fa75b3c842
minoka kakizaki <minoka.kakizaki@gmail.com>
2021-12-06 14:25:08 -0800

    got the blog to populate

--- src/App.js
+++ src/App.js
@@ -8,10 +8,11 @@ import { getBlogs } from './services/blog';
 import { useState, useEffect } from 'react';
 
 function App() {
-  const [blog, setBlog] = useState({});
+  const [blog, setBlog] = useState([]);
   useEffect(() => {
     const fetchData = async () => {
       const data = await getBlogs();
+      console.log(data);
       setBlog(data);
     };
     fetchData();
@@ -20,7 +21,11 @@ function App() {
   return (
     <section className="main">
       <Header />
-      <BlogCard {...{ blog }} />
+
+      {blog.map((post) => {
+        return <BlogCard key={post.id} {...post} />;
+      })}
+
       <Footer />
     </section>
   );
